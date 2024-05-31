f224907f8d45ae5b0487e43600c2ab660808c9b8
unknown <kalyaninalamaru.com>
2024-05-31 17:46:09 +0530

    first commit

---
+++ variable2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 : Hello $PERSON2, Good morning"
+echo "$PERSON2 : Hi $PERSON1 ,very good morning"
+echo "$PERSON1 :how are you $PERSON2?"
+echo "$PERSON2 : Iam good $PERSON1 , How are you"
+
