0ae39ad3d38a26853fadbf0960423b4e8af4d130
unknown <kalyaninalamaru.com>
2024-06-01 08:41:31 +0530

    first commit

--- array.sh
+++ array.sh
@@ -1,13 +1,3 @@
-# #!/bin/bash
-
-# FRUITS = ("Apple" "Banana" "mango")
-
-# echo "First Value : ${FRUITS[0]}"
-# echo "second value : ${FRUITS[1]}"
-# echo "Third value :${FRUITS[2]}"
-
-
-
 #!/bin/bash
 
 FRUITS=("Apple" "Banana" "Mango")
