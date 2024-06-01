2101923c76a763fe92df2c4452875370ad819ce6
unknown <kalyaninalamaru.com>
2024-06-01 08:09:39 +0530

    first commit

--- argvariable.sh
+++ argvariable.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# PERSON=$1
-# PERSON=$2
+PERSON=$1
+PERSON=$2
 
-# echo "$PERSON1 : Hello $PERSON2 , Good Morning"
-# echo "$PERSON2 : Hi $PERSON1, Very Good Morning"
-# echo "$PERSON1 : How are you $PERSON2?"
-# echo "$PERSON2 : Iam good $PERSON1, How are you"
+echo "$PERSON1 : Hello $PERSON2 , Good Morning"
+echo "$PERSON2 : Hi $PERSON1, Very Good Morning"
+echo "$PERSON1 : How are you $PERSON2?"
+echo "$PERSON2 : Iam good $PERSON1, How are you"
 
-PERSON1=$1
-PERSON2=$2
+# PERSON1=$1
+# PERSON2=$2
 
-echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, Very Good Morning"
-echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I am good $PERSON1, How are you?"
+# echo "$PERSON1: Hello $PERSON2, Good Morning"
+# echo "$PERSON2: Hi $PERSON1, Very Good Morning"
+# echo "$PERSON1: How are you $PERSON2?"
+# echo "$PERSON2: I am good $PERSON1, How are you?"
