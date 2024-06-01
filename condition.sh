c3168db4356b6d9e36ed7a71fc1d419756e42f86
unknown <kalyaninalamaru.com>
2024-06-01 08:51:07 +0530

    first commit

--- condition.sh
+++ condition.sh
@@ -1,12 +1,25 @@
 #!bin/bash
 
+# NUMBER=$1
+# if [ $NUMBER -gt 100 ];
+# then
+#   echo "Give number $NUMBER is greater than 100"
+
+#   else
+
+#   echo "Give number $NUMBER is not greater than 100"
+
+#   fi
+
+
+
+
+#!/bin/bash
+
 NUMBER=$1
-if [ $NUMBER -gt 100 ];
-then
-  echo "Give number $NUMBER is greater than 100"
 
-  else
-
-  echo "Give number $NUMBER is not greater than 100"
-
-  fi
+if [ "$NUMBER" -gt 100 ]; then
+  echo "Given number $NUMBER is greater than 100"
+else
+  echo "Given number $NUMBER is not greater than 100"
+fi
