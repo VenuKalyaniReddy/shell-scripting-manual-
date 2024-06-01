5c8e03821a7b9761842a402e71341947782bcb43
unknown <kalyaninalamaru.com>
2024-06-01 07:41:19 +0530

    first commit

---
+++ argvariable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON=$1
+PERSON=$2
+
+echo "$PERSON1:Hello $PERSON2 , Good Morning"
+echo "$PERSON2: Hi $PERSON1, Very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: Iam good $PERSON1, How are you"
+
