c210b59452c0e1beaf8108b2b50ff8c005b19f04
unknown <kalyaninalamaru.com>
2024-06-03 22:20:46 +0530

    first commit

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-NUMBER =$1
-NUMBER =$2
+NUMBER1=$1
+NUMBER2=$2
 
-SUM=$(( $NUMBER+$NUMBER ))
+SUM=$(($NUMBER1+$NUMBER2))
     echo "Total :: $SUM"
     echo "How many args passed :: $#"
     echo "All args passed :: $@"
