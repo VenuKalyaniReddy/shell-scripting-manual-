3b3c132f462fe9c42e849f632d0b591fd668fc09
unknown <kalyaninalamaru.com>
2024-06-03 18:14:17 +0530

    first commit

---
+++ 08-install-mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+
+then 
+    echo "Error :: Please run this script with root access"
+
+else
+    
+    echo "you are root user"
+
+fi
+
+yum install mysql -y
+
