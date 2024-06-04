ba506d81e461a8d1193b6b0c286bf411afb98ab8
unknown <kalyaninalamaru.com>
2024-06-04 09:05:25 +0530

    first commit

--- 12-logfile.sh
+++ 12-logfile.sh
@@ -20,24 +20,16 @@ VALIDATE () {
 if [ $ID -ne 0 ]
 
 then 
-
     echo "Error:: Please run this script as a rot user"
-
     exit 1
-
 else
-
     echo "you are a root user"
-
 fi
 
-    yum install mysql -y &>> $LOGFILE
-    
-    VALIDATE $? "INSTALLING MYSQL"
-
-    yum install git -y &>> $LOGFILE
-
-    VALIDATE $? "INSTALLING GIT"
+yum install mysql -y &>> $LOGFILE
+VALIDATE $? "INSTALLING MYSQL"
+yum install git -y &>> $LOGFILE
+VALIDATE $? "INSTALLING GIT"
 
 
 
