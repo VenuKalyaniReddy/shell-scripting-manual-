4ad11c011d5314ebebec8ec07db9aef81ddab01a
unknown <kalyaninalamaru.com>
2024-06-04 19:12:01 +0530

    first commit

--- 09-exit-install-git.sh
+++ 09-exit-install-git.sh
@@ -6,7 +6,7 @@ if [ $ID -ne 0 ]
 then
     echo "Error:Please run this script with root access"
 
-    exit1
+    exit 1
 else
 
     echo "you are root user"
@@ -20,10 +20,12 @@ if [ $? -ne 0 ]
 then 
 
     echo "Error:: Installing GIT is Failed"
-    exit1
+    exit 1
 
 else
 
     echo "Installing GIT is Success"
 
 fi
+
+
