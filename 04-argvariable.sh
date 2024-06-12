9332dfe9bc7859ded55e7daf215846a0f5ec48b2
unknown <kalyaninalamaru.com>
2024-06-12 12:32:42 +0530

    roboshop shell scripts

--- 04-argvariable.sh
+++ 04-argvariable.sh
@@ -8,10 +8,4 @@ echo "$PERSON2: Hi $PERSON1, Very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: Iam good $PERSON1, How are you"
 
-# PERSON1=$1
-# PERSON2=$2
 
-# echo "$PERSON1: Hello $PERSON2, Good Morning"
-# echo "$PERSON2: Hi $PERSON1, Very Good Morning"
-# echo "$PERSON1: How are you $PERSON2?"
-# echo "$PERSON2: I am good $PERSON1, How are you?"
