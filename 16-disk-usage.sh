e11949ef2c613c3682ada1d2ffff511cb8ba885d
unknown <kalyaninalamaru.com>
2024-06-12 14:50:46 +0530

    roboshop shell scripts

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -7,7 +7,8 @@ while IFS= read line
 do
     Usage=$(echo $line | awk '{print $6F}'|cut -d %f1)
     Partition=$(echo $line | awk '{print $1F}')
-    if [ $Usage -gt $DISK_THERSHOLD ]
+    #if [ $Usage -gt $DISK_THERSHOLD ]
+    if [ $usage -ge $DISK_THRESHOLD ]
     then 
         message+= "High disk usage on $Partition: $Usage\n"
     fi
